acacd5a826cb5355ead84b5dfb2c3383d7dba01f
Balram Kumar <balramkgg99@gmail.com>
2022-08-03 16:01:53 +0530

    display array element using break

---
+++ Array/peintFirstThreeElementUsingBreak.js
@@ -0,0 +1,10 @@
+// frint first three element using break 
+
+let fruits = ["Mango", "Banana", "Apple","kiwi", "grapes"];
+for(var i=0;i<fruits.length;i++){
+    if(i==3){
+        break;
+    }
+    console.log(fruits[i]);
+
+}
