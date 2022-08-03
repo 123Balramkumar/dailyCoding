b8fd7841c516b6c091dc881e30c2a8387fadbb69
Balram Kumar <balramkgg99@gmail.com>
2022-08-03 15:22:21 +0530

    add last element in an array

---
+++ Array/addLastElement.js
@@ -0,0 +1,4 @@
+// Add last element in an array
+const element =["apple", "banana","Grapes"];
+element.push("Mango");
+console.log(element);
--- Array/removeLastElement.js
+++ Array/removeLastElement.js
@@ -2,3 +2,4 @@
 const myArr=[1,2,4,6,5,8,9,4];
 myArr.pop();
 console.log(myArr);
+
