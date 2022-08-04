23686aac39f788092867129cdd7368d6a3646ada
Balram Kumar <balramkgg99@gmail.com>
2022-08-04 14:11:08 +0530

    print last three element

--- Array/peintFirstThreeElementUsingBreak.js
+++ Array/peintFirstThreeElementUsingBreak.js
@@ -1,10 +1,9 @@
-// frint first three element using break 
+// frint first three element using break
 
-let fruits = ["Mango", "Banana", "Apple","kiwi", "grapes"];
-for(var i=0;i<fruits.length;i++){
-    if(i==3){
-        break;
-    }
-    console.log(fruits[i]);
-
+let fruits = ["Mango", "Banana", "Apple", "kiwi", "grapes"];
+for (var i = 0; i < fruits.length; i++) {
+  if (i == 3) {
+    break;
+  }
+  console.log(fruits[i]);
 }
---
+++ Array/peintLastThreeElementUsingBreak.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango", "Banana", "Apple", "kiwi", "grapes"];
+var start = fruits.length-3;
+for(i=start; i<fruits.length;i++){
+    console.log(fruits[i]);
+}
