54c0316b9fbab8619bff00b8401e7f49eae190bc
Balram Kumar <balramkgg99@gmail.com>
2022-08-05 15:47:45 +0530

    average of even number

---
+++ Array/average.js
@@ -0,0 +1,12 @@
+// average of an eve number 
+var num = [12,9,4,3,1,45,5];
+var sum =0;
+var count = 0;
+for( var i=0;i< num.length; i++){
+if(num[i]%2==0){
+    sum =sum + num[i];
+    count++;
+
+}
+}
+console.log("Average = "+sum/count);
